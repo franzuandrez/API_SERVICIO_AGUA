7177f1011065bf6b3543b110e3d05226caa7e95f
Edson Andrez <edson.andrez@outlook.es>
2020-05-05 22:08:26 -0600

    Modelo de Motivo de visita o no visita

---
+++ API_SERVICIO_AGUA/Models/Motivo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SERVICIO_AGUA.Models
+{
+    public class Motivo
+    {
+        public int id_motivo { get; set; }
+        public string descripcion { get; set; }
+
+    }
+}
