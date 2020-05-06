32aa600da2de5fee6c6684a5382b24985755c854
Edson Andrez <edson.andrez@outlook.es>
2020-05-05 22:29:20 -0600

    Modelo Foto

---
+++ API_SERVICIO_AGUA/Models/Foto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SERVICIO_AGUA.Models
+{
+    public class Foto
+    {
+        [Key]
+        public int id_foto { get; set; }
+        public string path { get; set; }
+
+        public int id_orden { get; set; }
+
+        public Orden orden { get; set; }
+    }
+}
