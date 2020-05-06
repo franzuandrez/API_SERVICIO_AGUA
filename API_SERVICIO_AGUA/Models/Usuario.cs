04e74cd569e874567a4e67dd55d58ff1555e9ca4
Edson Andrez <edson.andrez@outlook.es>
2020-05-05 22:11:10 -0600

    Modelo Usuario

---
+++ API_SERVICIO_AGUA/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SERVICIO_AGUA.Models
+{
+    public class Usuario
+    {
+
+        public int id_usuario { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        
+
+    }
+}
