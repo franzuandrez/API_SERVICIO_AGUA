2bf3f44534d028b686760cdd3f6527a5d3e63c86
Edson Andrez <edson.andrez@outlook.es>
2020-05-05 22:00:13 -0600

    Agregar archivos de proyecto.

---
+++ API_SERVICIO_AGUA/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SERVICIO_AGUA.Models
+{
+    public class Cliente
+    {
+        public int id_cliente { get; set; }
+        public string nit { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string direccion { get; set; }
+    }
+}
